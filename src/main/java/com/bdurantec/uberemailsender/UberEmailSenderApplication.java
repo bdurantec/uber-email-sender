134870c34486b57ac511656a830247ff128c04f1
bdurantec <brunodurantec@gmail.com>
2023-09-26 20:21:02 -0300

    First commit

---
+++ src/main/java/com/bdurantec/uberemailsender/UberEmailSenderApplication.java
@@ -0,0 +1,13 @@
+package com.bdurantec.uberemailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberEmailSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberEmailSenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bdurantec/uberemailsender/UberEmailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdurantec.uberemailsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberEmailSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
